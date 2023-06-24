0adc219005b1a25e00bb5fdbbe0f145fdb27e3f6
electrovir <electrovir@users.noreply.github.com>
2023-06-24 14:53:20 +0000

    ugprade deps and configs

--- cspell.config.js
+++ cspell.config.js
@@ -4,6 +4,7 @@ module.exports = {
     ...baseConfig,
     ignorePaths: [
         ...baseConfig.ignorePaths,
+        '**/book-dist/',
     ],
     words: [
         ...baseConfig.words,
--- packages/vira/configs/vite.config.ts
+++ packages/vira/configs/vite.config.ts
@@ -4,4 +4,7 @@ import {defineConfig} from 'vite';
 export default defineConfig({
     ...baseViteConfig,
     base: process.env.CI ? '/vira' : '',
+    build: {
+        outDir: './book-dist',
+    },
 });

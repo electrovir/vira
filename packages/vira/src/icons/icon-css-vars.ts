005a1e106c7e8e03b4b897edee3cfa21b003abfc
electrovir <electrovir@users.noreply.github.com>
2023-08-17 13:13:41 +0000

    remove CSS var vira-icon-color, default vira-icon-fill-color to none

--- packages/vira/src/icons/icon-css-vars.ts
+++ packages/vira/src/icons/icon-css-vars.ts
@@ -1,24 +1,9 @@
 import {defineCssVars} from 'lit-css-vars';
 
-const genericIconColorCssVar = defineCssVars({
-    /**
-     * Specifies the icon color as a whole. This will color both the icon stroke and icon fill
-     * colors unless those respective CSS vars are specifically overridden. This CSS var value
-     * defaults to "currentColor", so in most cases you don't even need to override this variable,
-     * you just need to change the icon's "color" property.
-     */
-    'vira-icon-color': 'currentColor',
-});
-
-const specificIconCssVars = defineCssVars({
+export const viraIconCssVars = defineCssVars({
     /** To be used for coloring an icon's stroke. */
-    'vira-icon-stroke-color': genericIconColorCssVar['vira-icon-color'].value,
+    'vira-icon-stroke-color': 'currentColor',
     /** To be used for coloring an icon's fill. */
-    'vira-icon-fill-color': genericIconColorCssVar['vira-icon-color'].value,
+    'vira-icon-fill-color': 'none',
     'vira-icon-stroke-width': '1px',
 });
-
-export const viraIconCssVars = {
-    ...genericIconColorCssVar,
-    ...specificIconCssVars,
-};

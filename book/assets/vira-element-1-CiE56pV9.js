d24c4ddb616cbaca7674cd25bc967a2ba32ddf56
electrovir <electrovir@users.noreply.github.com>
2026-04-01 15:11:28 +0000

    Deploying to gh-pages from @ electrovir/vira@e67fbb71ac5f7587121846b3a46de7a90bbaed4d 🚀

--- book/assets/vira-element-1-CiE56pV9.js
+++
@@ -1,3 +0,0 @@
-import{i as e,t}from"./vir-html-C0N0bJrN.js";var n=e()({tagName:`vira-element-1-mock`,render(){return t`
-            First element
-        `}});export{n as ViraElement1Mock};
--- book/assets/vira-element-2-BX3gnVym.js
+++
@@ -1,3 +0,0 @@
-import{i as e,t}from"./vir-html-C0N0bJrN.js";var n=e()({tagName:`vira-element-2-mock`,render({inputs:e}){return t`
-            Second element ${e.userName}
-        `}});export{n as ViraElement2Mock};
